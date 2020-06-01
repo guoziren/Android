4e91c26b6934e016117038f6a4bd5b9519c35cde
guoziren <234917515@qq.com>
2020-06-01 19:56:34 +0800

    android仓库以及动态加载project demo

---
+++ AndroidDynamicLoad/README.md
@@ -0,0 +1,4 @@
+# AndroidDynamicLoad
+样例工程
+1. 如何动态化加载java代码
+2. 如何加载apk资源
---
+++ AndroidDynamicLoad/mypluginapk/src/main/java/com/example/mypluginapk/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mypluginapk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+}
